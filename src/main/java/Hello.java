cf4e55dfcf9d6120edafab0ffc7c085afdf7a7d2
basha <basha.shk10@yahoo.com>
2020-04-24 08:27:01 +0530

    aded gradle

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
   
-  public static String GREETING = "Hello world!";
+  public static String GREETING = "This is your application!";
   
   public static void main(String []args) {
     
